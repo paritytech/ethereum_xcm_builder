94fa7e89dc0a3b0136e4375f823a3bfe6928e9d2
Daniel Bigos <daniel.bigos@parity.io>
2023-03-14 14:00:04 +0100

    chore: upgrade deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,6 @@
+ # exit when any command fails
+set -e
+
 NODE_RPC_URL="http://127.0.0.1:8545"
 DEPLOYER="0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
 BYTES="Bytes"
@@ -7,9 +10,20 @@ COMPACTTYPES="CompactTypes"
 CALLENCODER="CallEncoder"
 XCMBUILDER="XCMBuilder"
 
-forge create --rpc-url ${NODE_RPC_URL} --private-key "${DEPLOYER}" "src/utils/${MEMORY}.sol:${MEMORY}"
-forge create --rpc-url ${NODE_RPC_URL} --private-key "${DEPLOYER}" "src/utils/${BYTES}.sol:${BYTES}"
-forge create --rpc-url ${NODE_RPC_URL} --private-key "${DEPLOYER}" "src/utils/${SCALECODEC}.sol:${SCALECODEC}"
-forge create --rpc-url ${NODE_RPC_URL} --private-key "${DEPLOYER}" "src/${COMPACTTYPES}.sol:${COMPACTTYPES}"
-forge create --rpc-url ${NODE_RPC_URL} --private-key "${DEPLOYER}" "src/${CALLENCODER}.sol:${CALLENCODER}"
-forge create --rpc-url ${NODE_RPC_URL} --private-key "${DEPLOYER}" "src/${XCMBUILDER}.sol:${XCMBUILDER}"
+
+
+deploy () {
+    contract=$1
+    echo "------------------------------------------------------------------------------------"
+    echo "Deploying contract: ${contract} ..."
+    forge create --rpc-url ${NODE_RPC_URL} --private-key "${DEPLOYER}" "${contract}"    
+    echo "Contract: ${contract} deployed successfully!"
+    echo "------------------------------------------------------------------------------------\n"
+}
+
+deploy "src/utils/${MEMORY}.sol:${MEMORY}"
+deploy "src/utils/${BYTES}.sol:${BYTES}"
+deploy "src/utils/${SCALECODEC}.sol:${SCALECODEC}"
+deploy "src/${COMPACTTYPES}.sol:${COMPACTTYPES}"
+deploy "src/${CALLENCODER}.sol:${CALLENCODER}"
+deploy "src/${XCMBUILDER}.sol:${XCMBUILDER}"
--- node.sh
+++ node.sh
@@ -2,5 +2,6 @@ MNEMONIC="test test test test test test test test test test test junk"
 DERIVATION_PATH="m/44'/60'/0'/0/"
 PORT=8545
 ACCOUNTS_NO=10
+CHAIN_ID=1337
 
-anvil --mnemonic "${MNEMONIC}" --derivation-path "${DERIVATION_PATH}" --port ${PORT} --accounts ${ACCOUNTS_NO}
+anvil --mnemonic "${MNEMONIC}" --derivation-path "${DERIVATION_PATH}" --port ${PORT} --accounts ${ACCOUNTS_NO} --chain-id ${CHAIN_ID}
